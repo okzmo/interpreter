c149d185d290787fcdce1163dd1043239eec1d47
okzmo <mathieurossi77@gmail.com>
2024-02-24 14:24:05 +0100

    inti

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/okzmo/interpreter
+
+go 1.22.0
---
+++ token/token.go
@@ -0,0 +1,55 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL   = "ILLEGAL" // token we don't know about
+	EOF       = "EOF"
+	IDENT     = "IDENT" // Name of function, variables, etc...
+	INT       = "INT"
+	ASSIGN    = "="
+	PLUS      = "+"
+	MINUS     = "-"
+	SLASH     = "/"
+	ASTERISK  = "*"
+	LT        = "<"
+	GT        = ">"
+	BANG      = "!"
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	LET       = "LET"
+	FUNCTION  = "FUNCTION"
+	RETURN    = "RETURN"
+	IF        = "IF"
+	ELSE      = "ELSE"
+	TRUE      = "TRUE"
+	FALSE     = "FALSE"
+	EQ        = "=="
+	NOT_EQ    = "!="
+)
+
+var keywords = map[string]TokenType{
+	"let":    LET,
+	"fn":     FUNCTION,
+	"return": RETURN,
+	"if":     IF,
+	"else":   ELSE,
+	"true":   TRUE,
+	"false":  FALSE,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
